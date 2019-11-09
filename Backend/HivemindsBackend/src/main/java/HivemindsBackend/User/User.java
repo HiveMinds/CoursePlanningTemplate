6cfa111b00bf32ed7a78d06bc2abf73ff4e2334f
a-t-0 <eai1@protonmail.com>
2019-11-09 22:54:15 +0100

    Created user.

--- Backend/HivemindsBackend/src/main/java/HivemindsBackend/User/User.java
+++ Backend/HivemindsBackend/src/main/java/HivemindsBackend/User/User.java
@@ -1,33 +1,72 @@
 package HivemindsBackend.User;
 
-import HivemindsBackend.EduStructure.BachelorCatalog;
 import HivemindsBackend.EduStructure.Course;
-import HivemindsBackend.EduStructure.CourseCatalog;
-import HivemindsBackend.EduStructure.FacultyCatalog;
-import HivemindsBackend.EduStructure.MasterCatalog;
-import HivemindsBackend.EduStructure.UniCatalog;
-
 import java.util.ArrayList;
 
+import HivemindsBackend.CourseProperties.ExamQuestion;
 import HivemindsBackend.CourseProperties.OldExam;
 
 public class User {
 	private String hashcode;
 	private String githubAccount;
+	private OldExam examToSubmit;
+	private ArrayList<ExamQuestion> examQuestionsReviewOne;
+	private ArrayList<ExamQuestion> examQuestionsReviewTwo;
 	private String OverLeafLink;
 
-	// These fields might already be contained in a uni catalog (hence might be
-	// removed)
-	private FacultyCatalog facultyCatalog;
-	private MasterCatalog masterCalog;
-	private BachelorCatalog bachelorCatalog;
-	private CourseCatalog courseCatalog;
-
-
 	/**
-	 * Represents a user that can attend multiple courses at multiple universities
+	 * Represents a user that can attend multiple courses at multiple universities.
 	 */
 	public User(String hashcode) {
-		
+		this.hashcode = hashcode;
 	}
+
+	public String getHashcode() {
+		return hashcode;
+	}
+
+	public void setHashcode(String hashcode) {
+		this.hashcode = hashcode;
+	}
+
+	public String getGithubAccount() {
+		return githubAccount;
+	}
+
+	public void setGithubAccount(String githubAccount) {
+		this.githubAccount = githubAccount;
+	}
+
+	public OldExam getExamToSubmit() {
+		return examToSubmit;
+	}
+
+	public void setExamToSubmit(OldExam examToSubmit) {
+		this.examToSubmit = examToSubmit;
+	}
+
+	public String getOverLeafLink() {
+		return OverLeafLink;
+	}
+
+	public void setOverLeafLink(String overLeafLink) {
+		OverLeafLink = overLeafLink;
+	}
+
+	public ArrayList<ExamQuestion> getExamQuestionsReviewOne() {
+		return examQuestionsReviewOne;
+	}
+
+	public void setExamQuestionsReviewOne(ArrayList<ExamQuestion> examQuestionsReviewOne) {
+		this.examQuestionsReviewOne = examQuestionsReviewOne;
+	}
+
+	public ArrayList<ExamQuestion> getExamQuestionsReviewTwo() {
+		return examQuestionsReviewTwo;
+	}
+
+	public void setExamQuestionsReviewTwo(ArrayList<ExamQuestion> examQuestionsReviewTwo) {
+		this.examQuestionsReviewTwo = examQuestionsReviewTwo;
+	}
+	
 }
